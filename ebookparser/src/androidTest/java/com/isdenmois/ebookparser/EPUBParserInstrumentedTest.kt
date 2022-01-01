200887b31d4eb3a15c609e4813b6fed744004938
Denis Moiseev <isdenmois@gmail.com>
2022-01-01 22:39:02 +0300

    Use strikt library for assertions

--- ebookparser/src/androidTest/java/com/isdenmois/ebookparser/EPUBParserInstrumentedTest.kt
+++ ebookparser/src/androidTest/java/com/isdenmois/ebookparser/EPUBParserInstrumentedTest.kt
@@ -1,53 +1,72 @@
 package com.isdenmois.ebookparser
 
+import strikt.api.expectThat
+import strikt.assertions.*
+import strikt.java.*
+
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 @RunWith(AndroidJUnit4::class)
 class EPUBParserInstrumentedTest {
     @Test
     fun parseEpubPlain() {
         val file = TestFileLoader.loadFile("plain.epub")
-        assertTrue(file.exists())
+
+        expectThat(file).exists()
+        expectThat(file).isRegularFile()
 
         val eBookFile = EBookParser.parseBook(file)
 
-        assertNotNull(eBookFile!!)
-        assertEquals("A Darker Shade of Magic", eBookFile.title)
-        assertEquals("V.E Schwab", eBookFile.author)
-        assertNotNull(eBookFile.cover)
-        assertNotNull(eBookFile.file)
+        expectThat(eBookFile).isNotNull()
+
+        expectThat(eBookFile!!.title).isEqualTo("A Darker Shade of Magic")
+        expectThat(eBookFile.author).isEqualTo("V.E Schwab")
+        expectThat(eBookFile.cover).isNotNull()
+        expectThat(eBookFile.file).isSameInstanceAs(file)
     }
 
     @Test
     fun parseEpubWithArtWorkOnly() {
         val file = TestFileLoader.loadFile("artwork.epub")
-        assertTrue(file.exists())
+        expectThat(file).exists()
 
         val eBookFile = EBookParser.parseBook(file)
 
-        assertNotNull(eBookFile!!)
-        assertEquals("The Stand", eBookFile.title)
-        assertEquals("Stephen King", eBookFile.author)
-        assertNotNull(eBookFile.cover)
-        assertNotNull(eBookFile.file)
+        expectThat(eBookFile).isNotNull()
+
+        expectThat(eBookFile!!.title).isEqualTo("The Stand")
+        expectThat(eBookFile.author).isEqualTo("Stephen King")
+        expectThat(eBookFile.cover).isNotNull()
     }
 
     @Test
     fun parseEpubNoCoverId() {
         val file = TestFileLoader.loadFile("no-cover-id.epub")
-        assertTrue(file.exists())
+        expectThat(file).exists()
 
         val eBookFile = EBookParser.parseBook(file)
 
-        assertNotNull(eBookFile!!)
-        assertEquals("Gentlemen and Players", eBookFile.title)
-        assertNotNull(eBookFile.author)
-        assertNotNull(eBookFile.cover)
-        assertNotNull(eBookFile.file)
+        expectThat(eBookFile).isNotNull()
+
+        expectThat(eBookFile!!.title).isEqualTo("Gentlemen and Players")
+        expectThat(eBookFile.author).isEqualTo("Joanne Harris")
+        expectThat(eBookFile.cover).isNotNull()
+    }
+
+    @Test
+    fun parseWithNoCover() {
+        val file = TestFileLoader.loadFile("empty-cover.epub")
+        expectThat(file).exists()
+
+        val eBookFile = EBookParser.parseBook(file)
+
+        expectThat(eBookFile).isNotNull()
+
+        expectThat(eBookFile!!.title).isEqualTo("Look what I've done")
+        expectThat(eBookFile.author).isEqualTo("Some Weirdo")
+        expectThat(eBookFile.cover).isNull()
     }
 }
--- ebookparser/src/androidTest/java/com/isdenmois/ebookparser/FB2ParserInstrumentedTest.kt
+++ ebookparser/src/androidTest/java/com/isdenmois/ebookparser/FB2ParserInstrumentedTest.kt
@@ -5,35 +5,58 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import strikt.api.expectThat
+import strikt.assertions.*
+import strikt.java.*
 
 @RunWith(AndroidJUnit4::class)
 class FB2ParserInstrumentedTest {
     @Test
     fun parsePlain() {
         val file = TestFileLoader.loadFile("murakami.fb2")
-        assertTrue(file.exists())
+
+        expectThat(file).exists()
+        expectThat(file).isRegularFile()
 
         val eBookFile = EBookParser.parseBook(file)
 
-        assertNotNull(eBookFile!!)
-        assertEquals("Норвежский лес", eBookFile.title)
-        assertEquals("Харуки Мураками", eBookFile.author)
-        assertNotNull(eBookFile.cover)
-        assertNotNull(eBookFile.file)
+        expectThat(eBookFile).isNotNull()
+
+        expectThat(eBookFile!!.title).isEqualTo("Норвежский лес")
+        expectThat(eBookFile.author).isEqualTo("Харуки Мураками")
+        expectThat(eBookFile.cover).isNotNull()
+        expectThat(eBookFile.file).isSameInstanceAs(file)
     }
 
     @Test
     fun parseZip() {
         val file = TestFileLoader.loadFile("hp1.fb2.zip")
-        assertTrue(file.exists())
+
+        expectThat(file).exists()
+        expectThat(file).isRegularFile()
 
         val eBookFile = EBookParser.parseBook(file)
 
-        assertNotNull(eBookFile!!)
-        assertEquals("Гарри Поттер и философский камень", eBookFile.title)
-        assertEquals("Джоан Роулинг", eBookFile.author)
-        assertNotNull(eBookFile.cover)
-        assertNotNull(eBookFile.file)
+        expectThat(eBookFile).isNotNull()
+
+        expectThat(eBookFile!!.title).isEqualTo("Гарри Поттер и философский камень")
+        expectThat(eBookFile.author).isEqualTo("Джоан Роулинг")
+        expectThat(eBookFile.cover).isNotNull()
+    }
+
+    @Test
+    fun parseWithNoCover() {
+        val file = TestFileLoader.loadFile("empty-cover.fb2")
+
+        expectThat(file).exists()
+        expectThat(file).isRegularFile()
+
+        val eBookFile = EBookParser.parseBook(file)
+
+        expectThat(eBookFile).isNotNull()
+
+        expectThat(eBookFile!!.title).isEqualTo("Все ок")
+        expectThat(eBookFile.author).isEqualTo("Макс Фрай")
+        expectThat(eBookFile.cover).isNull()
     }
 }
